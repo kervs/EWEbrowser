8b2d1964be34767289bafda761017feba0615a6d
Kervins Valcourt <kervins.valcourt@gmail.com>
2014-08-06 22:31:07 -0400

    Initial Commit

---
+++ Browser/EWEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EWEAppDelegate.h
+//  Browser
+//
+//  Created by Kervins Valcourt on 8/6/14.
+//  Copyright (c) 2014 EastoftheWestEnd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EWEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Browser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Browser
+//
+//  Created by Kervins Valcourt on 8/6/14.
+//  Copyright (c) 2014 EastoftheWestEnd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EWEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EWEAppDelegate class]));
+    }
+}
